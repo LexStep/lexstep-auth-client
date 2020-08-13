984c627035edb42557ac6f7c61aed7c20cac4ca1
Rai Butera <rai@rbutera.com>
2020-08-13 16:26:06 +0100

    feat(auth0): add environment variables

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
   env: {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
   },
 }
